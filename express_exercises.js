fd12a04481c0d3e849b9bd312f96077320c9e201
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-12-06 14:11:36 -0500

    Finished

--- express_exercises.js
+++ express_exercises.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
-// const time = require('time');
+// const bodyParser = require('body-parser');
+
+app.use(express.static('views'));
+// app.use(bodyParser.json());
+app.set('view engine', 'hbs');
+
+// Exercise 3
+app.get('/name/:name/age/:age', function(req, res) {
+  var name = req.params.name;
+  var age = req.params.age;
+  var d = new Date().getFullYear();
+  var bornIn = d - age;
+
+  res.render('index.hbs', {
+    name: name,
+    bornIn: bornIn
+  });
+});
+
+
+
+app.listen(3000, function() {
+  console.log('Listening on *:3000');
+});
+
+
 
 //Exercise 1
 // app.get('/', function(req, res) {
@@ -14,16 +39,3 @@ const app = express();
 //   var bornIn = d - age;
 //   res.send('You were born in ' + bornIn);
 // });
-
-// Exercise 3
-app.get('/name/:name/age/:age', function(req, res) {
-  var name = req.params.name || 'world';
-  var age = req.params.age;
-  var d = new Date().getFullYear();
-  var bornIn = d - age;
-  res.send('Hello, ' + name + '! You were born in  ' + bornIn);
-});
-
-app.listen(3000, function() {
-  console.log('Listening on *:3000');
-});
