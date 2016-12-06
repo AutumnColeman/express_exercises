cd1125477e2f01f696e70214df750f792c5db139
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-12-06 13:52:37 -0500

    Up to handlebars

--- express_exercises.js
+++ express_exercises.js
@@ -2,14 +2,26 @@ const express = require('express');
 const app = express();
 // const time = require('time');
 
+//Exercise 1
 // app.get('/', function(req, res) {
 //   res.send('Hello World!');
 // });
-app.get('/year', function(req, res) {
-  var age = req.query.age;
+
+//Exercise 2
+// app.get('/year', function(req, res) {
+//   var age = req.query.age;
+//   var d = new Date().getFullYear();
+//   var bornIn = d - age;
+//   res.send('You were born in ' + bornIn);
+// });
+
+// Exercise 3
+app.get('/name/:name/age/:age', function(req, res) {
+  var name = req.params.name || 'world';
+  var age = req.params.age;
   var d = new Date().getFullYear();
-  bornIn = d - age;
-  res.send('You were born in ' + bornIn);
+  var bornIn = d - age;
+  res.send('Hello, ' + name + '! You were born in  ' + bornIn);
 });
 
 app.listen(3000, function() {
